2bf88ff486a717c114e6b629a5a37f064d79c1ea
janakhpon <minchanhtutoo@gmail.com>
2019-05-29 17:08:32 +0630

    init

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Portfolio
+# TUvoice (Beata release)
+  This is a beta release of Technological Universities Related webpage for suggestions and updates in TU. Avialble to all students who want to share outlook and suggestions on how TU should be formed and upgraded for the better future.
+  Users must be sign up to use this app with email address and share the topics, contents and #notes to tuVoice.
+  Feedback and Help options are availe for positive feedbacks and suggestions on System and Designs on how to be better and also if user ran into errors, user can inform developers directly what to do and how to do.
+  Using multi languages display for internalization so user could be more relaxed concerning languages.. Here is a github example demo for [herokudeploy](https://ei-sgallery-41a7f.firebaseapp.com/)
+
+
+
+# Usage
+
+clone the repository
+
+    git clone https://github.com/janakhpon/tureleaseone.git
+
+Get into root folder
+
+    cd tureleaseone
+
+Install Dependencies using [npm](https://www.npmjs.com/)
+
+    npm install
+    npm i -d
+    npm start
+
+Install Dependencies using [yarn](https://yarnpkg.com/en/)
+
+    yarn install
+    yarn add
+    yarn start
+
+
+
+
+# Warning
+If u run into error use the following steps
+
+```bash
+sudo yarn install
+sudo yarn add
+sudo yarn start
+```
+or
+
+```bash
+sudo npm install
+sudo npm i -d
+sudo npm start
+```
+
+
+
+# INFO
+TUVOICE IS A WORK OF UNIVERSITY OF TECHNOLOGY (MAWLAMYINE PROJECT)
+
--- admin.php
+++ admin.php
@@ -41,7 +41,7 @@
   </div>
   <div class="page">
 
-  <nav id="colorlib-main-nav" role="navigation">
+    <nav id="colorlib-main-nav" role="navigation">
       <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle active"><i></i></a>
       <div class="js-fullheight colorlib-table">
         <div class="img" style="background-image: url(images/author-2.svg);"></div>
@@ -54,8 +54,8 @@
                 <li><a href="about.php"><span><small>02</small>About</span></a></li>
                 <li><a href="services.php"><span><small>03</small>Services</span></a></li>
                 <li><a href="portfolio.php"><span><small>04</small>Portfolio</span></a></li>
-								<li><a href="contact.php"><span><small>05</small>Contact</span></a></li>
-								<li><a href="auth.php"><span><small>06</small>Auth</span></a></li>
+                <li><a href="contact.php"><span><small>05</small>Contact</span></a></li>
+                <li><a href="auth.php"><span><small>06</small>Auth</span></a></li>
                 <li><a href="projects.php"><span><small>07</small>Projects</span></a></li>
               </ul>
             </div>
@@ -72,7 +72,7 @@
             <div class="col-md-12">
               <div class="colorlib-navbar-brand">
                 <a class="colorlib-logo" href="index.html"><span class="logo-img" style="background-image: url(images/person_1.jpg);"></span>Janakh
-                  Pon</a>
+                Pon</a>
               </div>
               <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
             </div>
@@ -106,59 +106,59 @@
           
           if(isset($_SESSION['id'])){
 
-      
-          
-          ?>
+            
+            
+            ?>
 
 
 
 
 
-<div class="row block-9">
+            <div class="row block-9">
 
 
-        <div class="col-md-6 pr-md-5">
-        <form method="post" action="func_insert.php" enctype="multipart/form-data">
-            <div class="form-group">
-              <input type="text"  class="form-control" name="image" placeholder="Image URL">
+              <div class="col-md-6 pr-md-5">
+                <form method="post" action="func_insert.php" enctype="multipart/form-data">
+                  <div class="form-group">
+                    <input type="text"  class="form-control" name="image" placeholder="Image URL">
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="language" placeholder="Main Language">
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="name" placeholder="Project Name">
+                  </div>
+                  <div class="form-group">
+                    <textarea cols="30" rows="10" class="form-control" name="description" placeholder="Project Description"></textarea>
+                  </div>
+
+                  
+                </div>
+
+
+
+                <div class="col-md-6 pr-md-5">
+
+                  <div class="form-group">
+                    <select class="form-control" name="hosted">
+                      <option selected disabled>Hosted or Deployed ?</option>
+                      <option value="YES">YES</option>
+                      <option value="NO">NO</option>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="link" placeholder="Deployed application Link">
+                  </div>
+                  <div class="form-group">
+                    <input type="submit" name="submit" value="SAVE" class="btn btn-primary py-3 px-5">
+                  </div>
+                </form>
+              </div>
+
+
+
+
             </div>
-            <div class="form-group">
-              <input type="text" class="form-control" name="language" placeholder="Main Language">
-            </div>
-            <div class="form-group">
-              <input type="text" class="form-control" name="name" placeholder="Project Name">
-            </div>
-            <div class="form-group">
-              <textarea cols="30" rows="10" class="form-control" name="description" placeholder="Project Description"></textarea>
-            </div>
-
-        
-        </div>
-
-
-
-        <div class="col-md-6 pr-md-5">
-
-            <div class="form-group">
-                <select class="form-control" name="hosted">
-                            <option selected disabled>Hosted or Deployed ?</option>
-                            <option value="YES">YES</option>
-                            <option value="NO">NO</option>
-                </select>
-            </div>
-            <div class="form-group">
-              <input type="text" class="form-control" name="link" placeholder="Deployed application Link">
-            </div>
-            <div class="form-group">
-              <input type="submit" name="submit" value="SAVE" class="btn btn-primary py-3 px-5">
-            </div>
-            </form>
-        </div>
-
-
-
-
-</div>
 
 
 
@@ -167,12 +167,12 @@
           <?php }else { ?>
 
             
-          <div class="row block-9">
-          <div class="col-md-6 pr-md-5">
+            <div class="row block-9">
+              <div class="col-md-6 pr-md-5">
 
-          <h1>ACCESS DENIED</h1>
-          </div>
-          </div>
+                <h1>ACCESS DENIED</h1>
+              </div>
+            </div>
 
 
           <?php } ?>
@@ -202,11 +202,11 @@
           <div class="row">
             <div class="col-md-12 text-center">
 
-            <p>
+              <p>
                 Copyright &copy;
                 <script>document.write(new Date().getFullYear());</script> All rights reserved | Made
                 with <i class="icon-heart" aria-hidden="true" style="color: red;"></i> by <a href="https://github.com/janakhpon"
-                  target="_blank">Janakhpon</a>
+                target="_blank">Janakhpon</a>
               </p>
             </div>
           </div>
@@ -215,32 +215,32 @@
 
       <!-- loader -->
       <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
-          <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
-          <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" /></svg></div>
+        <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+        <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" /></svg></div>
+
+      </div>
 
     </div>
 
-  </div>
 
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/scrollax.min.js"></script>
+    <script src="js/bootstrap-datepicker.js"></script>
+    <script src="js/jquery.timepicker.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+    <script src="js/google-map.js"></script>
+    <script src="js/main.js"></script>
 
-  <script src="js/jquery.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/jquery.waypoints.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.animateNumber.min.js"></script>
-  <script src="js/scrollax.min.js"></script>
-  <script src="js/bootstrap-datepicker.js"></script>
-  <script src="js/jquery.timepicker.min.js"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="js/google-map.js"></script>
-  <script src="js/main.js"></script>
+  </body>
 
-</body>
-
-</html>
+  </html>
